ceb2b1f03bb776a9465ccb0a27277119fea481c7
Tetiana Petrishena <petrisenatetana92@gmail.com>
2026-08-24 23:53:46 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Article from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 function App() {
   return (
-    <>
+    <main className="app">
       <Welcome />
       <Header />
       <Article />
-    </>
+    </main>
   );
 }
 
